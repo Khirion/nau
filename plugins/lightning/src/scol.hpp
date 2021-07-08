7a82c776b00990db60f2175747a7db6373132e50
Kirion <nreis007@gmail.com>
2021-07-08 01:09:19 +0100

    Works but is very slow for some reason...
    Thinking of ways to rewrite and optimize the code.

--- plugins/lightning/src/scol.hpp
+++ plugins/lightning/src/scol.hpp
@@ -25,20 +25,6 @@ struct node {
         scan(true)
     {};
 
-    node(glm::vec3 _pos) :
-        parent(_pos),
-        pos(_pos),
-        dir(0,-1,0),
-        scan(true)
-    {};
-
-    node(glm::vec3 _pos, glm::vec3 _dir) :
-        parent(_pos),
-        pos(_pos),
-        dir(_dir),
-        scan(true)
-    {};
-
     node(glm::vec3 _parent, glm::vec3 _pos, glm:: vec3 _dir) :
         parent(_parent),
         pos(_pos),
@@ -56,18 +42,21 @@ struct node {
 struct charge {
     glm::vec3 pos;
     glm::vec3 closestNode;
-    float kd;
 
-    charge(glm::vec3 _pos, float _kd) :
+    charge() :
+        pos(glm::vec3(0, 0, 0)),
+        closestNode(glm::vec3(FLT_MAX, FLT_MAX, FLT_MAX))
+    {};
+
+    charge(glm::vec3 _pos) :
         pos(_pos),
-        closestNode(FLT_MAX, FLT_MAX, FLT_MAX),
-        kd(_kd)
+        closestNode(glm::vec3(FLT_MAX, FLT_MAX, FLT_MAX))
     {};
 
     ~charge() {}
 
-    bool operator==(const charge& c) const {
-        return (all(equal(c.pos, pos)));
+    bool operator==(const glm::vec3& p) const {
+        return (all(equal(pos, p)));
     }
 };
 
@@ -75,16 +64,14 @@ class scol {
 private:
     std::vector<node> tree;
     std::list<charge> charges;
-    std::vector<charge> waypoints;
 
 public:
     scol() {};
     std::vector<glm::vec3> getVertices();
     std::vector<unsigned int> getIndices();
 
-    void init(float killDst, float attDst, int sphereNum, int chargeNum, int gLength, std::vector<glm::vec3> ways);
+    void init(float killDst, float attDst, int chargeNum, int sphereNum, int gLength, std::vector<glm::vec3> ways);
     void grow();
-    void addWaypoints(std::vector<glm::vec3> ways);
     void genCharges(glm::vec3 root, glm::vec3 way, int sphereNum, int chargeNum);
     glm::vec3 randdir(glm::vec3 vec);
     bool updateAttractors();
